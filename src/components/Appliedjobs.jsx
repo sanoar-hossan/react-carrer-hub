7ec1a2ca63f23b1567d83b1959fc82811473dd74
chayan <admin@ditskill.com>
2023-04-10 01:45:24 +0600

    header setup

---
+++ src/components/Appliedjobs.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Appliedjobs = () => {
+    return (
+        <div>
+            <h2>applided</h2>
+        </div>
+    );
+};
+
+export default Appliedjobs;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,33 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './components/Home'
+import Blog from './components/Blog'
+import Appliedjobs from './components/Appliedjobs'
+
+const router=createBrowserRouter([{
+path:'/',
+element:<Home></Home>,
+children: [
+  {
+path:'/',
+element: <Home></Home>,
+  },
+  {
+path:'blog',
+element: <Blog></Blog>,
+  },
+
+  {
+path:'jobs',
+element: <Appliedjobs></Appliedjobs>,
+  },
+],
+
+}])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
